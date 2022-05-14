d8d5ce5ef29458534b8feaad2e55e0c2588c6819
MMnaimi <mm.naimi@yahoo.com>
2022-05-14 15:31:12 +0430

    Transactions page ui refactor

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,18 +1,18 @@
-// console.log('working')
-// const signup = document.querySelector('#sign-up-section')
-// const signin = document.querySelector('#sign-in-section')
-// document.querySelector('#signin-item').addEventListener('click',function(){
-//     signup.style.display = "none"
-//     signin.style.display = "block"
-// })
-// document.querySelector("#signup-item").addEventListener('click', function(){
-//     signin.style.display = "none"
-//     signup.style.display = "block"
-// })
+console.log('working')
+const signup = document.querySelector('#sign-up-section')
+const signin = document.querySelector('#sign-in-section')
+document.querySelector('#signin-item').addEventListener('click',function(){
+    signup.style.display = "none"
+    signin.style.display = "block"
+})
+document.querySelector("#signup-item").addEventListener('click', function(){
+    signin.style.display = "none"
+    signup.style.display = "block"
+})
 
-// document.querySelector("body").addEventListener('click', function(){
-//     document.querySelector(".message_area").style.display = 'none'
-// })
+document.querySelector("body").addEventListener('click', function(){
+    document.querySelector(".message_area").style.display = 'none'
+})
 function show_password(){
     const toggle_show_passwd = document.querySelector('#togglePassword');
     const password = document.querySelector('#login_password');
